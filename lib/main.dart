8011886c075e7d46348b7d3ce4d83fccd03a5968
Yash Chandra <yash24.chandra2003@gmail.com>
2026-01-08 15:01:24 +0530

    Added Splash Screen, Starting to work on Login Screen

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:recipe_finder_app/features/splash/presentation/splash_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(textTheme: GoogleFonts.poppinsTextTheme()),
+      home: const SplashScreen(),
+    );
+  }
+}
